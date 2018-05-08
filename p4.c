0fe1394e96b478d9361f892fe337e9925a60374d
jrkb6 <sbneov@gmail.com>
2018-05-09 01:08:15 +0300

    fcfs std completed

--- p4.c
+++ p4.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 //struct for the store input file information with corresponding list
-typedef struct node{
+typedef struct node
+{
     struct node *next;
     int arrival_time;
     int cylinder;
     int exec_start;
-}node;
+    int waiting_time;
+} node;
 //erdem
 //headers
 void readfile(char **argv);
-void fcfs(node *requestlist,int *totalBlockChange, int *averageWaitingTime,int *stdWaitTime);
-void sstf(node *requestlist,int *totalBlockChange, int *averageWaitingTime,int *stdWaitTime);
-void look(node *requestlist,int *totalBlockChange, int *averageWaitingTime,int *stdWaitTime);
-void clook(node *requestlist,int *totalBlockChange, int *averageWaitingTime,int *stdWaitTime);
-void addNode(node **head,int time,int cylinder;);
+void fcfs(node *requestlist, int *totalBlockChange, double *averageWaitingTime, double *stdWaitTime);
+void sstf(node *requestlist, int *totalBlockChange, double *averageWaitingTime, double *stdWaitTime);
+void look(node *requestlist, int *totalBlockChange, double *averageWaitingTime, double *stdWaitTime);
+void clook(node *requestlist, int *totalBlockChange, double *averageWaitingTime, double *stdWaitTime);
+void addNode(node **head, int time, int cylinder);
+double stdDeviation(node* list, double average);
 
 //addnode for linkedlist
-void addNode(node **head,int time,int cylinder){
+void addNode(node **head, int time, int cylinder)
+{
     //printf("adding val:  %d %d ",time,cylinder;);
-    if ((*head) == NULL){
+    if ((*head) == NULL)
+    {
         (*head) = malloc(sizeof(node));
         (*head)->arrival_time = time;
         (*head)->cylinder = cylinder;
         (*head)->exec_start = 0;
+        (*head)->waiting_time = 0;
         (*head)->next = NULL;
     }
-    else{
+    else
+    {
         node *temp = (*head);
-        while(temp->next != NULL){
+        while (temp->next != NULL)
+        {
             temp = temp->next;
         }
-        node *newNode =malloc(sizeof(node));
+        node *newNode = malloc(sizeof(node));
         newNode->arrival_time = time;
         newNode->cylinder = cylinder;
         newNode->exec_start = 0;
@@ -43,35 +52,38 @@ void addNode(node **head,int time,int cylinder){
 
 //lists for input readings
 node *requestList = NULL;
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     readfile(argv);
-    node *temp = requestList;
     //passing parameters
     int totalBlockChange;
-    int averageWaitingTime;
-    int stdWaitTime;
+    double averageWaitingTime;
+    double stdWaitTime;
 
     //function calls
-    fcfs(requestList,&totalBlockChange,&averageWaitingTime,&stdWaitTime);
+    fcfs(requestList, &totalBlockChange, &averageWaitingTime, &stdWaitTime);
 
     //printing results
-    printf("FCFS result= %d \t%d\t%d\n",totalBlockChange,averageWaitingTime,stdWaitTime);
+    printf("FCFS result= %d \t %.2f\t%f\n", totalBlockChange, averageWaitingTime, stdWaitTime);
     return 0;
 }
 //reading from file
-int numberofrequest= 0;
-void readfile(char **argv){
+int numberofrequest = 0;
+void readfile(char **argv)
+{
     //printf("Reading file\n");
     FILE *handler;
-    handler= fopen(argv[2],"r");
+    handler = fopen(argv[2], "r");
     int t;
-    
+
     int cnt = 0;
-    while(fscanf(handler,"%d",&t) == 1){
+    while (fscanf(handler, "%d", &t) == 1)
+    {
         int cylinder;
-        if(fscanf(handler,"%d",&cylinder) == 1){
+        if (fscanf(handler, "%d", &cylinder) == 1)
+        {
             //printf("t: %d blk: %d \n",t,cylinder;);
-            addNode(&requestList,t,cylinder);
+            addNode(&requestList, t, cylinder);
             numberofrequest++;
         }
         cnt++;
@@ -79,91 +91,74 @@ void readfile(char **argv){
     //printf("closing file");
     fclose(handler);
 }
-void fcfs(node *requestlist,int *totalBlockChange, int *averageWaitingTime,int *stdWaitTime){    //assuming we're 1 initial. So first comer is also included in calculation.
-    node *temp  = requestlist;
+
+double stdDeviation(node* list, double average)
+{
+    double sum = 0;
+    node* temp = list;
+    while(temp!=NULL)
+    {
+        sum += pow(temp->waiting_time - average,2);
+        temp = temp->next;
+    }
+   
+    return sqrt(sum/numberofrequest);
+}
+
+void fcfs(node *requestlist, int *totalBlockChange, double *averageWaitingTime, double *stdWaitTime){ //assuming we're at 1 initial. So first comer is also included in calculation.
+    node *temp = requestlist;
     (*totalBlockChange) = 0;
     (*averageWaitingTime) = 0;
     (*stdWaitTime) = 0;
     int prev = 1;
     int passedTime = 0;
-    while ( temp != NULL){
-        (*totalBlockChange)  += abs(prev-temp->cylinder);
-        if(passedTime <= temp->arrival_time){
-            passedTime = (*totalBlockChange) + temp->arrival_time;
+    while (temp != NULL)
+    {
+        (*totalBlockChange) += abs(prev - temp->cylinder);
+        if (passedTime <= temp->arrival_time)
+        {
+            passedTime = abs(prev - temp->cylinder) + temp->arrival_time;
         }
-        else {
-            averageWaitingTime += passedTime - temp->arrival_time;
-            passedTime += (*totalBlockChange);
+        else
+        {   
+            temp->waiting_time = (passedTime - temp->arrival_time);
+            (*averageWaitingTime) += temp->waiting_time;
+            passedTime += abs(prev - temp->cylinder);
         }
-
         prev = temp->cylinder;
         temp = temp->next;
     }
     (*averageWaitingTime) /= numberofrequest;
-    
+    (*stdWaitTime) = stdDeviation(requestList,(*averageWaitingTime));
 }
-void sstf(node *requestlist,int *totalBlockChange, int *averageWaitingTime,int *stdWaitTime){    //assuming we're 1 initial. So first comer is also included in calculation.
-    node *temp  = requestlist;
-    (*totalBlockChange) = 0;
-    (*averageWaitingTime) = 0;
-    int initial = 0;
-    (*stdWaitTime) = 0;
-    int prev = 0;
-    int differences[numberofrequest];
-    int index = 0;
-    while(temp != NULL && index < numberofrequest){
-        differences[index] = abs(initial - temp->next);
-        initial = temp->cylinder;
-        temp = temp->next;
-        index++;
-    }
-}
-//return the nearest next distance for the given node.
-int findShortest(node *reqlist){
-    node *temp;
-    if(reqlist->next){
-         temp = reqList->next;
-    }
-    else {
-        return 0;
-    }
-    int min = __INT_MAX__;
 
-    while(temp != NULL){
-        if(abs(temp->cylinder - reqlist->cylinder) < min){
-            min = abs(temp->cylinder - reqlist->cylinder);
-        }
-        temp = temp->next;
-        return min;
-
-    }
-}
-void look(node *requestlist,int *totalBlockChange, int *averageWaitingTime,int *stdWaitTime){    //assuming we're 0 initial. So first comer is also included in calculation.
-    node *temp  = requestlist;
+void look(node *requestlist, int *totalBlockChange, double *averageWaitingTime, double *stdWaitTime)
+{ //assuming we're 0 initial. So first comer is also included in calculation.
+    node *temp = requestlist;
     (*totalBlockChange) = 0;
     (*averageWaitingTime) = 0;
     (*stdWaitTime) = 0;
     int prev = 0;
-    while ( temp != NULL){
-        (*totalBlockChange)  += abs(prev-temp->cylinder);
+    while (temp != NULL)
+    {
+        (*totalBlockChange) += abs(prev - temp->cylinder);
         prev = temp->cylinder;
 
-
         temp = temp->next;
-    }   
-    
+    }
 }
-void clook(node *requestlist,int *totalBlockChange, int *averageWaitingTime,int *stdWaitTime){    //assuming we're 0 initial. So first comer is also included in calculation.
-    node *temp  = requestlist;
+void clook(node *requestlist, int *totalBlockChange, double *averageWaitingTime, double *stdWaitTime)
+{ //assuming we're 0 initial. So first comer is also included in calculation.
+    node *temp = requestlist;
     (*totalBlockChange) = 0;
     (*averageWaitingTime) = 0;
     (*stdWaitTime) = 0;
     int prev = 0;
-    while ( temp != NULL){
-        (*totalBlockChange)  += abs(prev-temp->cylinder);
+    while (temp != NULL)
+    {
+        (*totalBlockChange) += abs(prev - temp->cylinder);
         prev = temp->cylinder;
 
-
         temp = temp->next;
-    }   
+    }
 }
